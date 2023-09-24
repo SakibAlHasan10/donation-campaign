07a79ebb27a5219af2cb5c1c995b841cdeb468ee
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-25 05:10:46 +0600

    create card using loder data, set card backround color, category bg colorand text color, title text color

--- src/Components/Home/AllCard.jsx
+++ src/Components/Home/AllCard.jsx
@@ -1,19 +1,38 @@
-import PropTypes from 'prop-types'
-const AllCard = ({category}) => {
-    const {picture,title,category:item,card_bg,category_bg,text_color} = category
-    const cardBg = parseInt(card_bg)
-    console.log(category, cardBg, text_color)
-    return (
-        <div style={{backgroundColor: card_bg}} className='rounded-xl'>
-            <img src={picture} alt="" className='h-52 w-full rounded-t-xl'/>
-            <div className='px-5 pt-4 pb-5'>
-                <h2>{item}</h2>
-                <p>{title}</p>
-            </div>
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+const AllCard = ({ category }) => {
+  const {
+    picture,
+    title,
+    category: item,
+    card_bg,
+    category_bg,
+    text_color,
+  } = category;
+
+  return (
+    <div style={{ backgroundColor: card_bg }} className="rounded-xl">
+      <Link to={'/details'}>
+        <img src={picture} alt="" className="h-52 w-full rounded-t-xl" />
+        <div className="px-4 pt-4 pb-5">
+          <button
+            style={{ color: text_color, backgroundColor: category_bg }}
+            className={`capitalize text-sm font-medium py-1 px-4 rounded-md`}
+          >
+            {item}
+          </button>
+          <p
+            style={{ color: text_color }}
+            className=" mt-2 text-xl font-semibold"
+          >
+            {title}
+          </p>
         </div>
-    );
+      </Link>
+    </div>
+  );
 };
-AllCard.propTypes ={
-    category: PropTypes.object
-}
+AllCard.propTypes = {
+  category: PropTypes.object,
+};
 export default AllCard;
