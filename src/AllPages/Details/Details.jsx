c2ccdfc4a42d8145d69619e95cf318191f3142fe
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-25 12:49:37 +0600

    get and set localStorage data using js file

--- src/AllPages/Details/Details.jsx
+++ src/AllPages/Details/Details.jsx
@@ -9,11 +9,11 @@ const Details = () => {
     console.log(findCategory)
     const {picture, title, description, price,text_color} = findCategory
     return (
-        <div className="max-w-screen-xl mx-auto px-8 my-12">
+        <div className="max-w-screen-xl mx-auto px-8 my-1">
             <div className="relative">
                 <img src={picture} alt="" className="w-full h-[700px] rounded-3xl"/>
                 <div  className="flex w-full rounded-b-3xl absolute -mt-32 h-32 bg-[#00000047]">
-                    <button style={{backgroundColor: text_color}} className={`m-10  px-5 py-2 text-xl font-semibold rounded-md text-white`}>Donate ${price}</button>
+                    <button style={{backgroundColor: text_color}} className="m-10  px-5 py-2 text-xl font-semibold rounded-md text-white">Donate ${price}</button>
                 </div>
             </div>
             <h2 className="text-4xl font-bold mt-12 mb-6">{title}</h2>
