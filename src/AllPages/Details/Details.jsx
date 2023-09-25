53043425f87c33c2eb9b31ed36ffe9a148069f88
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-25 11:58:31 +0600

    add details image, button, image overlay in bottom and button backround color using of text color

--- src/AllPages/Details/Details.jsx
+++ src/AllPages/Details/Details.jsx
@@ -5,14 +5,19 @@ import { myContext } from "../../RootPages/RootPages";
 const Details = () => {
     const categories = useContext(myContext)
     const {id} = useParams()
-    const findCategory = categories.find(category => console.log(typeof(category.id)) )
-    console.log(typeof(+id))
+    const findCategory = categories.find(category => category.id === (+id) )
+    console.log(findCategory)
+    const {picture, title, description, price,text_color} = findCategory
     return (
-        <div>
-            {
-                
-            }
-            /donation/details
+        <div className="max-w-screen-xl mx-auto px-8 my-10">
+            <div className="relative">
+                <img src={picture} alt="" className="w-full h-[700px] rounded-3xl"/>
+                <div  className="flex w-full rounded-b-3xl absolute -mt-32 h-32 bg-[#00000047]">
+                    <button style={{backgroundColor: text_color}} className={`m-10  px-5 py-2 text-xl font-semibold rounded-md text-white`}>Donate ${price}</button>
+                </div>
+            </div>
+            <h2>{title}</h2>
+            <p>{description}</p>
         </div>
     );
 };
